d6faafe8a9114cea879d7aae8ef5fc0c392eb4b4
YlberKelmendi <ylber-kelmendi@hotmail.com>
2022-08-30 19:57:15 +0200

    ruaji keto

---
+++ Day24.js
@@ -0,0 +1,22 @@
+/*function addNums(x , y) {
+    return x + y 
+}
+console.log (addNums (7 , 9))*/
+
+function addNums(x, z, y, a) {
+    return `Im adding ${x}, ${z}, ${y}, ${a} and it gives us ${x + z +y +a}`
+}
+console.log (addNums(2 , 5 , 10 , 18))
+
+/*function multiplyNums(x , z, y) {
+    return x * z * y
+}
+alert(multiplyNums(12 , 5 , 2))
+
+function devideNums(x , z){
+    return x / z
+}
+console.log (devideNums(80 , 4))
+
+const sentence `Adding ${addNums($x , $z, $y, $a and it gives $(x + z + y +a))}`
+console.log(addNums (2 , 5, 10, 18))*/
