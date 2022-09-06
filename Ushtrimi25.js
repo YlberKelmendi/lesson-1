592a0d1c2c7b7833428df476eee11dfe9c6a154e
YlberKelmendi <ylber-kelmendi@hotmail.com>
2022-09-06 20:04:08 +0200

    ruaji keto

--- Ushtrimi25.js
+++ Ushtrimi25.js
@@ -1,8 +1,32 @@
-const button1 = document.getElementById("button1");
-function showtext1() {
-    const inputvalue = document.getElementById("inputi1").value;
-    const result = document.getElementById("result");
+// function showtext1() {
+//     const inputvalue = document.getElementById("inputi1").value;
+//     const result = document.getElementById("result");
 
-result.innerHTML = inputvalue 
+// result.innerHTML = "result: " + inputvalue
+// };
+// button1.addEventListener("click", showtext1);
+
+// const button2 = document.getElementById("button2");
+// function showtext2() {
+//     const inputvalue = document.getElementById("inputi2").value;
+//     const result = document.getElementById("result2");
+//     result.innerHTML = "result2 " + inputvalue
+
+// }
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+
+function showtext(inputId, textId) {
+  const inputvalue = document.getElementById(inputId).value;
+  const result = document.getElementById(textId);
+  result.innerHTML = "result1: " + inputvalue;
 }
-button1.addEventListener("click", showtext1)
+
+button1.addEventListener("click", function () {
+    showtext("input1","result1")
+} );
+
+button2.addEventListener("click", function () {
+    showtext("input2","result2")
+} );
