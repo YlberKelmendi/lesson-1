124559037b39a13f552671ba9e65f1af046fff50
YlberKelmendi <ylber-kelmendi@hotmail.com>
2022-09-20 19:49:40 +0200

    ruaji keto

---
+++ lesson30.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+//const car = {
+     //model: "S Model",
+    //brand: "Tesla",
+    //year: "2022", 
+
+   
+    
+
+//for(const key in car) {
+    //console.log("key:", key);
+   // console.log("value:", car[key]);
+
+
+/*const salaries = {
+    Jack: "24000",
+    Paul: "34000",
+    Monica: "55000",
+}
+for(const key in salaries) {
+    console.log("$" + salaries[key])
+    console.log("key:", salaries[key])
+}
+
+for (let index = 0; index < 5; index++) {
+    for (let index2 = 0; index2 < 3; index2++){
+        console.log("*");
+    }
+}*/
+
+/*const num = 7;
+let result = num;
+
+for (let index = 1; index < num; index++) {
+result = result * (num - index);
+}
+console.log("result", result);
+
+
+const array1 = ["apple", "orange", "banana"];
+console.log(array1.join(" - "));
+
+const string = "kjo eshte nje fjali";
+console.log(string.split(" "))*/
+
+const array1 = ["apple", "orange"];
+const array2 = ["kiwi"];
+
+console.log(array1.splice(2, 0, "banana"));
+console.log(array1)
+
